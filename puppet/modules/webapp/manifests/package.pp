21a77b9928d6b0d226ae00e6ba986eb24ae42d15
kkarthee <k.karthee@gmail.com>
2019-01-02 14:50:07 +0800

    test

--- puppet/modules/webapp/manifests/package.pp
+++ puppet/modules/webapp/manifests/package.pp
@@ -5,22 +5,25 @@
 # @example
 #   include webapp::package
 class webapp::package(
-  #$packages-pip = [ 'Flask' , 'Werkzeug' , 'python-virtualenv' ]
- # $packages-apt = []
+  #$packages-pip = [ 'Flask' , 'Werkzeug' , 'python-virtualenv' ],
+ $packages_apt = [ "build-essential", "python-dev" , "python-pip" , "python-flask" ],
 )
 {
-  exec { 'apt-get-update':
+  exec { 'apt_get_update':
   command => 'apt-get --yes update',
   path => ['/usr/bin', '/usr/sbin' , '/usr/local/bin' , '/usr/local/sbin' ,'/bin' , '/sbin'],
   } 
-  package { [ 'build-essential' , 'python-dev' , 'python-pip' , 'python-flask'] :
+  package {  $packages_apt:
+    
+  #  name => $packages-apt:
     ensure => 'present',
-    require => Exec['apt-get-update'],
+    provider => 'apt-get',
+    require => Exec['apt_get_update'],
   }
   package { [ 'virtualenv' , 'uwsgi'] :
   ensure => present,
   provider => pip,
-  #require => Package [ ]
+  require => Package[ $packages_apt ],
   }
 
  /* file { '/usr/bin/pip':
