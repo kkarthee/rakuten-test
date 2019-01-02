9bc5840f137c86ce7140fb07820cf728c1d706fc
kkarthee <k.karthee@gmail.com>
2019-01-02 20:22:29 +0800

    run_all.sh

--- puppet/modules/webapp/files/run_app.sh
+++ puppet/modules/webapp/files/run_app.sh
@@ -2,5 +2,10 @@
 cd /webapps/devops
  virtualenv env
  source env/bin/activate
- restart myproject
-#sudo service nginx restart  
+ if [ -f '/tmp/myproject.sock' ];
+  then
+  echo " My Project is  already started and it is running"
+  else
+  start myproject
+  fi
+#sudo service nginx restart
