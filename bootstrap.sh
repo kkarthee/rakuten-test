fa0e9d91df40346e20480c670794bcbdb654d466
Karthikeyan <k.karthee@gmail.com>
2019-01-03 20:41:04 +0800

    Update bootstrap.sh
    This script is not used anywhere. Commented out.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get --yes update
+/*sudo apt-get --yes update
 sudo apt-get --yes upgrade
 DEB_RELEASE=$(lsb_release -c -s)
       echo "Adding Puppet Labs repo for $DEB_RELEASE."
@@ -12,4 +12,4 @@ DEB_RELEASE=$(lsb_release -c -s)
       #curl http://pm.puppetlabs.com/puppet-agent/2018.1.5/5.5.8/repos/puppet-agent-ubuntu-16.04-amd64.tar.gz -o /tmp/puppet-agent-ubuntu-16.04-amd64.tar.gz
      # gunzip /tmp/puppet-agent-ubuntu-16.04-amd64.tar.gz
       apt-get -y -m update
-sudo apt-get --yes install puppet-agent
+sudo apt-get --yes install puppet-agent*/
